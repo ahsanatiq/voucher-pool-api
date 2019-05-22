3546223e920aa2ae85a95be9f0100774bf98a30b
Ahsan <me@ahsanatiq.info>
2019-05-22 16:12:43 +0500

    setup all routes

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,7 @@ $app->group('/api/v1', function ($app) {
     $app->get('/recipients', 'RecipientController:getList');
     $app->get('/recipients/vouchers', 'RecipientController:getVouchers');
     // Offers
-    // $app->get('/offers', OffersController::class . ':index');
-    // $app->post('/offers', OffersController::class . ':create');
+    $app->post('/offers', 'OfferController:create');
     // Voucher
-    // $app->post('/validate_voucher', VouchersController::class . ':validate');
+    $app->post('/redeem', 'VoucherController:redeem');
 });
