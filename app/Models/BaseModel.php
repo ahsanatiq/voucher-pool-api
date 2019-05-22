3b6fcff67746e7121a87866c65d05a5c2c9ef37c
Ahsan <me@ahsanatiq.info>
2019-05-22 16:16:38 +0500

    no need for it... already registering database inside application instance

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -5,10 +5,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    public static $instance;
-    public function __construct() {
-        if(!self::$instance) {
-            self::$instance = container()->get('eloquent');
-        }
-    }
+
 }
